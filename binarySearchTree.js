333702033e316b4d92bc817cc3a2b16656da6251
Kevin Turney <super996@optonline.net>
2017-04-28 12:12:10 -0400

    inOrder and preOrder traversal

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -34,3 +34,57 @@ BinarySearchTree.prototype.contains = function(value) {
   }
   return false;
 };
+
+var bsTree = new BinarySearchTree(10);
+bsTree.insert(5).insert(15).insert(8).insert(3).insert(7).insert(20).insert(17).insert(9).insert(14);
+
+
+// In Order Traversal, most common
+/*
+Visit the left branch, then current node, then right branch
+for a binary search tree, this visits the nodes in secending order
+O(n)
+*/
+
+BinarySearchTree.prototype.traverseDepthFirst_inOrder = function(fn) { // we pass a fn to transform or operate on the node values
+  if (!this.left && !this.right) return fn(this);
+  if (this.left) this.left.traverseDepthFirst_inOrder(fn);
+  fn(this);
+  if (this.right) this.right.traverseDepthFirst_inOrder(fn);
+};
+
+var result_traverseDepthFirst_inOrder = [];
+bsTree.traverseDepthFirst_inOrder(function(node) {
+  result_traverseDepthFirst_inOrder.push(node.value);
+});
+console.log(result_traverseDepthFirst_inOrder, 'should be [3,5,7,8,9,10,14,15,17,20]');
+
+// Pre Order Traversal
+/*
+visits the current node before it's child nodes
+O(n)
+*/
+
+BinarySearchTree.prototype.traverseDepthFirst_preOrder = function(fn) {
+  fn(this);
+  if (this.left) this.left.traverseDepthFirst_preOrder(fn);
+  if (this.right) this.right.traverseDepthFirst_preOrder(fn);
+};
+
+var result_traverseDepthFirst_preOrder = [];
+bsTree.traverseDepthFirst_preOrder(function(node) {
+  result_traverseDepthFirst_preOrder.push(node.value);
+});
+console.log(result_traverseDepthFirst_preOrder, 'should be [10,5,3,8,7,9,15,14,20,17]');
+
+
+
+
+
+
+
+
+
+
+
+
