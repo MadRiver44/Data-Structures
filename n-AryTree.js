cfc787b920ae2be19d9ffca2516f64256384a66f
Kevin Turney <super996@optonline.net>
2017-05-10 22:34:10 -0400

    Basic Tree Implementation

--- n-AryTree.js
+++ n-AryTree.js
@@ -1,6 +1,40 @@
-// O(n)
+/*
+TREES, an abstract data type
+
+General Tree:
+  A tree hass a root node, The root node has zero or more children
+  Each child node has 0 or more children (each node in the tree can be seen as a subtree)
+
+Constraints:
+  A child node has only one parent, and the root node has no parent
+  A tree is a soecial type of graph, A tree is a graph without cycles
+
+*/
 
 // N-ary tree - any number of children
+function Tree(value) {
+  this.value = value;
+  this.children = [];
+}
+
+// Adds child  to tree or subtree bound to 'this' keyword
+// O(1)
+Tree.prototype.addChild = function(value) {
+  var child = new Tree(value);
+  this.children.push(child);
+  return child;
+};
+
+var tree = new Tree(1);
+var branch1 = tree.addChild(2);
+var branch2 = tree.addChild(3);
+var branch3 = tree.addchild(4);
+branch1.addChild(5);
+branch2.addChild(6);
+branch3.addchild(7).addChild(8);
+
+
+// O(n)
 Tree.prototype.contains = function(value) {
   if (this.value === value) return true;
   for (var i = 0; i < this.children.length; i++) {
@@ -42,4 +76,3 @@ tree.traverseBreadthFirst(function(node) {
 
 console.log(breadthFirstResult, 'should be [1,2,3,4,5,6,7,8]');
 
-tree;
